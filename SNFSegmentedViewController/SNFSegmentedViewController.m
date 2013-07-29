9b0f8e843e3f22b58ecb5628a476830e11b502bc
Seth Friedman <sethfri@gmail.com>
2013-07-29 10:00:43 -0500

    Fix initializer for use in .xibs

--- SNFSegmentedViewController/SNFSegmentedViewController.m
+++ SNFSegmentedViewController/SNFSegmentedViewController.m
@@ -16,20 +16,13 @@
 
 @implementation SNFSegmentedViewController
 
+#pragma mark - Initializers
+
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
     
     if (self) {
-        self.delegate = self;
-        
-        NSMutableArray *titles = [NSMutableArray array];
-        
-        for (UITabBarItem *tabBarItem in self.tabBar.items) {
-            [titles addObject:tabBarItem.title];
-        }
-        
-        _segmentedControl = [[UISegmentedControl alloc] initWithItems:[titles copy]];
-        self.navigationItem.titleView = _segmentedControl;
+        [self configureForInitialization];
     }
     
     return self;
@@ -39,28 +32,13 @@
     self = [super initWithCoder:aDecoder];
     
     if (self) {
-        self.delegate = self;
-        
-        NSMutableArray *titles = [NSMutableArray array];
-        
-        for (UITabBarItem *tabBarItem in self.tabBar.items) {
-            [titles addObject:tabBarItem.title];
-        }
-        
-        _segmentedControl = [[UISegmentedControl alloc] initWithItems:[titles copy]];
-        _segmentedControl.selectedSegmentIndex = self.selectedIndex;
-        
-#if __IPHONE_OS_VERSION_MIN_REQUIRED == __IPHONE_6_0 || __IPHONE_OS_VERSION_MIN_REQUIRED == __IPHONE_6_1
-        _segmentedControl.segmentedControlStyle = UISegmentedControlStyleBar;
-#endif
-        
-        [_segmentedControl addTarget:self action:@selector(segmentTapped:) forControlEvents:UIControlEventValueChanged];
-        
-        self.navigationItem.titleView = _segmentedControl;
+        [self configureForInitialization];
     }
     
     return self;
 }
+
+#pragma mark - View Controller Lifecycle
 
 - (void)viewDidLoad
 {
@@ -87,7 +65,28 @@
     self.selectedIndex = sender.selectedSegmentIndex;
 }
 
-#pragma mark - Helper Method
+#pragma mark - Helper Methods
+
+- (void)configureForInitialization {
+    self.delegate = self;
+    
+    NSMutableArray *titles = [NSMutableArray array];
+    
+    for (UITabBarItem *tabBarItem in self.tabBar.items) {
+        [titles addObject:tabBarItem.title];
+    }
+    
+    _segmentedControl = [[UISegmentedControl alloc] initWithItems:[titles copy]];
+    _segmentedControl.selectedSegmentIndex = self.selectedIndex;
+    
+#if __IPHONE_OS_VERSION_MIN_REQUIRED == __IPHONE_6_0 || __IPHONE_OS_VERSION_MIN_REQUIRED == __IPHONE_6_1
+    _segmentedControl.segmentedControlStyle = UISegmentedControlStyleBar;
+#endif
+    
+    [_segmentedControl addTarget:self action:@selector(segmentTapped:) forControlEvents:UIControlEventValueChanged];
+    
+    self.navigationItem.titleView = _segmentedControl;
+}
 
 // This borrows code from @bshirley at http://stackoverflow.com/questions/1982172/iphone-is-it-possible-to-hide-the-tabbar
 - (void)hideTabBar {
