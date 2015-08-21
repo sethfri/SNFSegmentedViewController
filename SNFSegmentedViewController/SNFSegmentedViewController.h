94237d0e4af0f1b6291401fd895843d5328bca0c
Kevin Randrup <krandrup@nuesoft.com>
2015-08-21 16:31:42 -0400

    Overrode UITabBarController methods so that they update the segmented control. Fixed a cash when the controller's title is nil. Documented the header file.

--- SNFSegmentedViewController/SNFSegmentedViewController.h
+++ SNFSegmentedViewController/SNFSegmentedViewController.h
@@ -8,6 +8,11 @@
 
 #import <UIKit/UIKit.h>
 
+/**
+ * SNFSegmentedViewController is the simplest way to use a UISegmentedControl to switch between view controllers.
+ * Just set the view controllers property in code or in a storyboard.
+ * The UIViewController's title is used for the segmented control. If the title is nil, an empty string is used instead.
+ */
 @interface SNFSegmentedViewController : UITabBarController
 
 @property (strong, nonatomic, readonly) UISegmentedControl *segmentedControl;
